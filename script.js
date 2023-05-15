b46fbe9b0440577fdb57d30a64517ea1dcdca2fc
kar-thik-m <124783120+Kar-thik-m@users.noreply.github.com>
2023-05-15 15:01:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+setTimeout(() => {
+    document.getElementById("sec").innerHTML = 10
+    setTimeout(() => {
+        document.getElementById("sec").innerHTML = 9
+        setTimeout(() => {
+            document.getElementById("sec").innerHTML = 8
+            setTimeout(() => {
+                document.getElementById("sec").innerHTML = 7
+                setTimeout(() => {
+                    document.getElementById("sec").innerHTML = 6
+                    setTimeout(() => {
+                        document.getElementById("sec").innerHTML = 5
+                        setTimeout(() => {
+                            document.getElementById("sec").innerHTML = 4
+                            setTimeout(() => {
+                                document.getElementById("sec").innerHTML = 3
+                                setTimeout(() => {
+                                    document.getElementById("sec").innerHTML = 2
+                                    setTimeout(() => {
+                                        document.getElementById("sec").innerHTML = 1
+
+                                        setTimeout(() => {
+                                            document.getElementById("sec").innerHTML = "HAPPY INDEPENDENCE DAY"
+                                        }, 1000);
+                                    }, 1000)
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
